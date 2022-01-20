797199349ab44fbdf0d19d2de287543df81b6a46
Charity Quinsay <cquinsay@gmail.com>
2022-01-20 14:50:54 -0800

    installed knex

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+knex({
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        port: 3306,
+        user: 'your_database_user',
+        password: 'your_database_password',
+        database: 'myapp_test'
+    }
+});
 
 const app = express();
 
