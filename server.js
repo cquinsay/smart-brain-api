0a26dc01f5a507167a971e5eec40b6691708a04b
Charity Quinsay <cquinsay@gmail.com>
2022-01-27 15:11:43 -0800

    updating db

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  }
 });
 
 const app = express();
