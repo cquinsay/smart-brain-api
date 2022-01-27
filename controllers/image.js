e7d5fae3089ceab602ae1d0c5ac9bd6f8eb3bff8
Charity Quinsay <cquinsay@gmail.com>
2022-01-27 13:10:50 -0800

    moved clarifai api to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,15 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'e717983978ad416b88e8dc9e44ff3e12'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0].entries);
-        })
-        .catch(err => res.status(400).json('unable to get entries'))
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
 
 }
 
 module.exports = {
-    handleImage
+  handleImage,
+  handleApiCall
 };
