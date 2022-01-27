a881a0c878cc5fc44c25c78913d6f4113e5f122c
Charity Quinsay <cquinsay@gmail.com>
2022-01-27 14:23:53 -0800

    updating db

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-rectangular-23457',
     user: 'cjquinsay',
     password: '',
     database: 'smart-brain'
