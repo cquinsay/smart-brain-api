8ad92fca38aa303265f7d328b30eedd7761fb8f2
Charity Quinsay <cquinsay@gmail.com>
2022-01-27 14:07:31 -0800

    added port

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-	client: 'pg',
-	connection: {
-		host: '127.0.0.1',
-		user: 'cjquinsay',
-		password: '',
-		database: 'smart-brain'
-	}
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'cjquinsay',
+    password: '',
+    database: 'smart-brain'
+  }
 });
 
 const app = express();
@@ -25,35 +25,35 @@ app.use(express.json());
 app.use(cors());
 
 const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			email: 'sally@gmail.com',
-			password: 'bananas',
-			entries: 0,
-			joined: new Date()
-		}
-	],
-	login: [
-		{
-			id: '987',
-			hash: '',
-			email: 'john@gmail.com'
-		}
-	]
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'john@gmail.com'
+    }
+  ]
 }
 
 app.get('/', (req, res) => {
-	res.send('Success');
+  res.send('Success');
 })
 
 app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) })
@@ -62,6 +62,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, () => {
-	console.log('app is running on Port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on Port ${process.env.PORT}`);
 })
